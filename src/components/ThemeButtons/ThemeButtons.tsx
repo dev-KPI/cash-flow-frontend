9c6832a910341b10fe373a2ff1360de1504ae490
Artem Zabozhko <artemko526@gmail.com>
2023-03-12 16:12:18 +0100

    Theme button update

--- src/components/ThemeButtons/ThemeButtons.tsx
+++ src/components/ThemeButtons/ThemeButtons.tsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from './ThemeButtons.module.css'
+//store
+import { useAppSelector, useAppDispatch } from '../../hooks/useAppStore'
+import { setAnotherTheme } from "../../store/ThemeSlice/ThemeSlice";
+
 export const ThemeButton = () => {
-    return (
-        <div className={classes.themeButton}>
-            <div className={classes.themeButton__inner}>
-                <div className={classes.themeButton__item + ' ' + classes.dark }>
-                    <i className="bi bi-moon"></i>
-                </div>
-                <div className={classes.themeButton__item + ' ' + classes.light + ' ' + classes.active}>
-                    <i className="bi bi-brightness-high"></i>
+
+    const actualTheme = useAppSelector((state) => state.ThemeSlice.type)
+    const dispatch = useAppDispatch();
+
+    const setTheme = () => {
+        const body = document.querySelector('body') as HTMLElement;
+        body.setAttribute('data-theme', actualTheme);
+    }
+    const updateTheme = async (e: React.ChangeEvent) => {
+        await dispatch(setAnotherTheme());
+        setTheme();
+    }
+
+    useEffect(() => {
+        const body = document.querySelector('body') as HTMLElement;
+        body.setAttribute('data-theme', actualTheme);
+    }, [])
+
+    return (<>
+        <input
+            type="checkbox"
+            id="themeToggle"
+            className={classes.themeButton__input}
+            onChange={updateTheme}
+        ></input>
+        <label
+            className={classes.themeButton__label}
+            htmlFor="themeToggle">
+            <div className={classes.themeButton}>
+                <div className={classes.themeButton__inner}>
+                    <div className={classes.themeButton__item + ' ' + classes.dark}>
+                        <i className="bi bi-moon"></i>
+                    </div>
+                    <div className={classes.themeButton__item + ' ' + classes.light + ' ' + classes.active}>
+                        <i className="bi bi-brightness-high"></i>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        </label>
+    </>)
 }
 
 export const ThemeButtonExtra = () => {
