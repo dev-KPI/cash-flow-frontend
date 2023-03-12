4f025e15f244b0bb2ceba7bec682b1502b13cafe
pest2811@gmail.com <pest2811@gmail.com>
2023-03-13 00:56:32 +0200

    Started dashboard page layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import MonthPicker from '../../components/MonthPicker/MonthPicker';
+import classes from './Dashboard.module.css'
 
 const Dashboard = () => {
     return (
         <div>
             <Header />
-            <h1>hi</h1>
+            <div className='dashboard__container'>
+                <div className={classes.header}>
+                    <h1 className={`${classes.title} pageTitle`}>Dashboard</h1>
+                    <MonthPicker />
+                </div>
+            </div>
         </div>
        
     );
