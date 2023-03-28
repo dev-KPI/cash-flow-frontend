9df787ab84bf17e04fce4c6295181d673a3ba38a
pest2811@gmail.com <pest2811@gmail.com>
2023-03-29 02:07:06 +0300

    Add method for formatting numbers

--- src/services/UsefulMethods/UsefulMethods.ts
+++ src/services/UsefulMethods/UsefulMethods.ts
@@ -2,3 +2,10 @@ export const Omiter = (key: string[], obj: Object) =>  {
     const exclude: Set<string> = new Set(key)
     return JSON.parse(JSON.stringify(Object.fromEntries(Object.entries(obj).filter(e => !exclude.has(e[0])))))
 }
+
+export function numberWithCommas(x: number | undefined) {
+    if (x)
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    else
+        return x
+}
