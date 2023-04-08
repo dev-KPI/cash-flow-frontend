3e6ecc06348dac109a7e0a921f9a0fa7c77de8a4
pest2811@gmail.com <pest2811@gmail.com>
2023-04-09 00:38:30 +0300

    wrapper method dev

--- src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
+++ src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import UserCategoriesCardDot from '../UserCategoriesCardDot/UserCategoriesCardDot';
 import classes from './UserCategoriesCard.module.css'
 
@@ -113,65 +113,116 @@ const json = {
     ],
 }
 
+interface Category {
+    id: number,
+    title: string,
+    color: string,
+    icon: string
+}
+
+export interface ICategoryItem {
+    category: Category,
+    amount: number
+}
+
 const UserCategoriesCard = () => {
+    const [categories, setCategories] = useState<ICategoryItem[]>([]);
+    const [totalCategories, setTotalCategories] = useState<number>(11);
     const {groups} = json;
 
-    const categories = groups[0].categories;
+    useEffect( () => {
+        setCategories(groups[0].categories);
+    }, [])
     
-    const getCategories = (categories: any[]) => {
-        return categories.map(item => <UserCategoriesCardDot key={item.category.id} category={item.category} amount={item.amount} />)
-    }
+    // var detectWrap = function (className: string) {
 
-    var detectWrap = function (className:string) {
+    //     // let wrappedItems = [];
+    //     let prevItem: DOMRect = {} as DOMRect;
+    //     let currItem: DOMRect = {} as DOMRect;
+    //     let items = document.getElementsByClassName(className);
 
-        // let wrappedItems = [];
-        let prevItem: DOMRect = {} as DOMRect;
-        let currItem: DOMRect = {} as DOMRect;
-        let items = document.getElementsByClassName(className);
+    //     for (var i = 0; i < items.length; i++) {
+    //         currItem = items[i].getBoundingClientRect();
+    //         if (prevItem && prevItem.top < currItem.top) {
+    //             items[i].classList.add("wrapped");
+    //         } else {
+    //             items[i].classList.remove("wrapped");
+    //         }
+    //         prevItem = currItem;
+    //     };
 
-        for (var i = 0; i < items.length; i++) {
-            currItem = items[i].getBoundingClientRect();
-            if (prevItem && prevItem.top < currItem.top) {
-                items[i].classList.add("wrapped");
-            } else {
-                items[i].classList.remove("wrapped");
-            }
-            prevItem = currItem;
-        };
+    //     // return wrappedItems;
 
-        // return wrappedItems;
+    // }
 
-    }
-
-    function detectWrap1(className: string) {
-        let container: HTMLElement = document.getElementsByClassName(className)[0] as HTMLElement;
+    function detectWrap(className: string) {
+        let container: HTMLElement = document.getElementsByClassName(className)[0] as HTMLElement
+        
         const gap = parseFloat(getComputedStyle(container).gap);
         for (const child of container.children) {
             const childElement = child as HTMLElement;
-            const offsetHeight = container.offsetTop + gap + childElement.offsetHeight
-            if (childElement.offsetTop > offsetHeight) {
-                childElement.classList.add("wrapped");
-            } else {
-                childElement.classList.remove("wrapped");
+            const prevSibling = childElement.previousSibling as HTMLElement;
+            childElement.classList.remove(classes.wrapped);
+            
+            const bigOffsetHeight = container.offsetTop + gap + childElement.offsetHeight
+            const lowOffsetHeight = container.offsetTop
+            
+            if (childElement.offsetTop > bigOffsetHeight) {
+                // console.log("1", childElement);
+                // console.log(prevSibling);
+                
+                prevSibling.classList.add(classes.wrapped);
+                if (!childElement.classList.contains("viewMore")) {
+                    childElement.classList.add(classes.wrapped);
+                }
+                
+                
+                
             }
+            // else if (childElement.offsetTop > lowOffsetHeight) {
+            //     // console.log("2", childElement); 
+            //     childElement.classList.remove(classes.wrapped);
+
+            //     // var rect = childElement.getBoundingClientRect();
+            //     // console.log(rect.top);
+            //     // console.log(offsetHeight);
+            // }
         }
     }
-    const setCategories = () => {
-        let container: HTMLElement = document.getElementsByClassName(classes.list)[0] as HTMLElement;
-        const list: HTMLCollectionOf<Element> = container.children;
-        const newList = Array.from(list).filter( (item:Element)  =>  !item.classList.contains("wrapped"));
-        newList.pop()
-        const viewMore: HTMLElement = document.querySelector(".viewMore") as HTMLElement;
-        newList.push(viewMore);
-        console.log(newList);
+    // const setCategories1 = () => {
+    //     let container: HTMLElement = document.getElementsByClassName(classes.list)[0] as HTMLElement;
+    //     const list: HTMLCollectionOf<Element> = container.children;
+    //     const newList = Array.from(list).filter( (item:Element)  =>  !item.classList.contains("wrapped"));
+    //     newList.pop()
+    //     // const viewMore: HTMLElement = document.querySelector(".viewMore") as HTMLElement;
+    //     // newList.push(viewMore);
+    //     // console.log(newList);
+    //     // console.log(newList.length);
+    //     setTotalCategories(newList.length)
     
 
-    }
+    // }
 
+    // const detectWrap = () => {
+    //     const elements= document.querySelectorAll(classes.list);
+    //     let previousElement: HTMLElement = {} as HTMLElement;
+    //     let rowTop = elements[0].getBoundingClientRect().top;
+    //     Array.from(elements).forEach(el => el.classList.remove('last-el-of-row'))
+    //     elements.forEach(el => {
+    //         const elementTop = el.getBoundingClientRect().top;
+    //         const elHTML = el as HTMLElement;
+    //         if (rowTop < elementTop) {
+    //             previousElement.classList.add('last-el-of-row');
+    //             rowTop = elementTop;
+    //         }
 
+    //         previousElement = elHTML;
+    //     })
+    // }
     window.addEventListener('resize', (event) =>{
-        detectWrap1(classes.list);
-        setCategories()
+        detectWrap(classes.list);
+        // setCategories1()
+        // console.log(totalCategories);
         // var wrappedItems = detectWrap(classes.item);
         // for (var k = 0; k < wrappedItems.length; k++) {
         //     wrappedItems[k].classList.add("wrapped");
@@ -179,6 +230,17 @@ const UserCategoriesCard = () => {
         // }
     });
 
+    const getCategories = (categories: any[]) => {
+        return categories.map((item, i) => <UserCategoriesCardDot key={i} category={item.category} amount={item.amount} />)
+    }
+    const useCategories = (categories: any[] , counter: number) => {
+        const properCategories = useMemo(() => {
+            return categories.slice(0, counter)
+        }, [categories, counter])
+        return properCategories;
+    }
+
+    const properCategories = useCategories(categories, totalCategories)
 
     return (
         <div className={classes.categories}>
@@ -191,7 +253,7 @@ const UserCategoriesCard = () => {
                     </div>
                 </div>
                 <ul className={classes.list}>
-                    {getCategories(categories)}
+                    {getCategories(properCategories)}
                     <li className={`${classes.item} viewMore`}>
                         <div className={classes.viewMore}>
                             <i className="bi bi-chevron-right"></i>
