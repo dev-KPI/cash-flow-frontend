326e4bbc04db683d20e8f01ebccaf32e44785345
pest2811@gmail.com <pest2811@gmail.com>
2023-04-12 02:10:35 +0300

    Add method for updating expenses by category

--- src/pages/Dashboard/UserCategoriesCardDot/UserCategoriesCardDot.tsx
+++ src/pages/Dashboard/UserCategoriesCardDot/UserCategoriesCardDot.tsx
@@ -1,5 +1,5 @@
 import { numberWithCommas } from '@services/UsefulMethods/UIMethods';
-import React, { FC, Dispatch, SetStateAction, useEffect } from 'react';
+import React, { FC, Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { ICategoryItem } from '../UserCategoriesCard/UserCategoriesCard';
 
 //UI
@@ -7,14 +7,31 @@ import classes from './UserCategoriesCardDot.module.css';
 
 
 const UserCategoriesCardDot: FC<ICategoryItem> = ({ category, amount}) => {
+    const [total, setTotal] = useState<number>();
+    const [source, setSource] = useState<string>();
+    
+    const updateAmount = (e: React.MouseEvent): void => {
+        e.preventDefault();
+        let newAmount = total
+        if (newAmount !== undefined)
+            newAmount += Number(prompt("Set new amount:"))
+        setTotal(newAmount);
+        setSource(prompt("Set source:") || '');
+    }
+
+    useEffect(() => {
+        setTotal(amount)
+    }, [])
 
     return (
-        <li className={classes.item}>
+        <li className={classes.item}
+            onClick={updateAmount}
+        >
             <h6 className={classes.expenseName}>{category.title}</h6>
             <div className={classes.icon} style={{background: category.color}}>
                 <i className="bi bi-credit-card-2-front"></i>
             </div>
-            <p className={classes.expenseAmount} style={{color: category.color}}>{`${numberWithCommas(amount)}$`}</p>
+            <p className={classes.expenseAmount} style={{ color: category.color }}>{`${numberWithCommas(total)}$`}</p>
         </li>
     );
 };
