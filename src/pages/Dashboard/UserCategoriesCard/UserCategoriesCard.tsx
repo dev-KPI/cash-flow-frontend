dc3f76d15a73dcaedf5de33fe41383f7adbecb53
pest2811@gmail.com <pest2811@gmail.com>
2023-04-12 02:13:30 +0300

    Styled disabled buttons and empty list

--- src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
+++ src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
@@ -167,6 +167,12 @@ const json = {
                     "amount": 59
                 }     
             ]
+        },
+        {
+            "title": "Empty",
+            "categories": [
+                
+            ]
         }
     ],
 }
@@ -191,25 +197,27 @@ const UserCategoriesCard = () => {
 
     const { categoriesByGroup } = json;
     let groups = categoriesByGroup.map(a => a.title);
+    
     useEffect( () => {
         const newCategories = categoriesByGroup.find(item => item.title === groups[groupIndex])?.categories
         if (newCategories)
             setCategories(newCategories);
     }, [groupIndex])
+
     function detectWrap(className: string) {
         let container: HTMLElement = document.getElementsByClassName(className)[0] as HTMLElement
-
+       
         const gap = parseFloat(getComputedStyle(container).gap);
         for (const child of container.children) {
             const childElement = child as HTMLElement;
             const prevSibling = childElement.previousElementSibling as HTMLElement;
 
             childElement.classList.remove(classes.wrapped);
-            const offsetHeight = container.offsetTop + gap + childElement.offsetHeight
-           
+
+            const offsetHeight = container.offsetTop + gap +  2 * childElement.offsetHeight
             if (childElement.offsetTop > offsetHeight) {
                 prevSibling.classList.add(classes.wrapped);
-                if (!childElement.classList.contains("viewMore")) {
+                if (!childElement.classList.contains(classes.specialItem)) {
                     childElement.classList.add(classes.wrapped);
                 }
             }
@@ -218,7 +226,7 @@ const UserCategoriesCard = () => {
 
     useEffect(()=> {
         detectWrap(classes.list);
-    }, [height, width])
+    }, [height, width, categories])
 
     const getCategories = (categories: ICategoryItem[]) => {
         return categories.map((item, i) => <UserCategoriesCardDot key={i} category={item.category} amount={item.amount} />)
@@ -232,12 +240,12 @@ const UserCategoriesCard = () => {
 
     const handleNextGroup = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (!groups[groupIndex + 1]) return;
-        setGroupIndex(groupIndex + 1)
+        setGroupIndex(groupIndex + 1); 
     }
 
     const handlePrevGroup =(e: React.MouseEvent<HTMLButtonElement>) => {
         if (!groups[groupIndex - 1]) return;
-        setGroupIndex(groupIndex - 1)
+        setGroupIndex(groupIndex - 1); 
     }
 
     const properCategories = useCategories(categories, totalItems)
@@ -251,25 +259,50 @@ const UserCategoriesCard = () => {
                         <button
                             type="submit"
                             onClick={handlePrevGroup}
-                            className={classes.btn + ' ' + classes.previous}>
+                            disabled={!groups[groupIndex - 1]}
+                            className={classes.btn + ' ' + classes.previous}
+                            >
                             <i id='chevron' className="bi bi-chevron-left"></i>
                         </button>
                         <button
                             type="submit"
                             onClick={handleNextGroup}
-                            className={classes.btn + ' ' + classes.next}>
+                            disabled={!groups[groupIndex + 1]}
+                            className={classes.btn + ' ' + classes.next}
+                            >
                             <i id='chevron' className="bi bi-chevron-right"></i>
                         </button>
                     </div>
                 </div>
                 <ul className={classes.list} ref={squareRef}>
                     {getCategories(properCategories)}
-                    <li className={`${classes.item} viewMore`}>
-                        <div className={classes.viewMore}>
-                            <i className="bi bi-chevron-right"></i>
-                        </div>
-                        <h6 className={classes.expenseName}>View More</h6>
-                    </li>
+                    {
+                        categories.length === 0 ?
+                            <div className={classes.emptyList}>
+                                <p>Category list is empty!</p>
+                                <li className={`${classes.item} ${classes.specialItem}`}>
+                                    <div className={classes.dashed}>
+                                        <i className="bi bi-plus-lg"></i>
+                                    </div>
+                                    <h6 className={classes.itemTitle}>Add More</h6>
+                                </li>
+                            </div> 
+                            :
+                       categories.length >= 11 ?
+                        <li className={`${classes.item} ${classes.specialItem}`}>
+                            <div className={classes.dashed}>
+                                <i className="bi bi-chevron-right"></i>
+                            </div>
+                            <h6 className={classes.itemTitle}>View More</h6>
+                        </li>
+                        :
+                        <li className={`${classes.item} ${classes.specialItem}`}>
+                            <div className={classes.dashed}>
+                                <i className="bi bi-plus-lg"></i>
+                            </div>
+                            <h6 className={classes.itemTitle}>Add More</h6>
+                        </li>
+                    }
                 </ul>    
             </div>
         </div>
