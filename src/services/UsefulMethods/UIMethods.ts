326ca1d0630739996579c26790c29a0dc281dacb
pest2811@gmail.com <pest2811@gmail.com>
2023-04-12 20:14:47 +0300

    Added handleWrap fucntion to useful methods

--- src/services/UsefulMethods/UIMethods.ts
+++ src/services/UsefulMethods/UIMethods.ts
@@ -21,3 +21,29 @@ export const parseColors = (hex: string) => {
             return hex
     }
 }
+
+export function handleWrap(
+    containerClass: string, 
+    wrappedClass: string,
+    specialItemClass:string,
+    rows: number) {
+    let container: HTMLElement = document.getElementsByClassName(containerClass)[0] as HTMLElement
+
+    const gap = parseFloat(getComputedStyle(container).gap);
+    for (const child of container.children) {
+        const childElement = child as HTMLElement;
+        const prevSibling = childElement.previousElementSibling as HTMLElement;
+
+        childElement.classList.remove(`${wrappedClass}`);
+
+        const offsetHeight = container.offsetTop + gap + rows * childElement.offsetHeight
+       
+        if (childElement.offsetTop >= offsetHeight) {
+            console.log(specialItemClass);
+            prevSibling.classList.add(`${wrappedClass}`);
+            if (!childElement.classList.contains(`${specialItemClass}`)) {
+                childElement.classList.add(`${wrappedClass}`);
+            }
+        }
+    }
+}
