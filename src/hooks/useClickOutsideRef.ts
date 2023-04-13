7b08b00b0b3129e672dc512244692202d8f68587
Artem Zabozhko <artemko526@gmail.com>
2023-04-14 00:05:34 +0200

    Hooks: added useClickOutsideRef

---
+++ src/hooks/useClickOutsideRef.ts
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from "react";
+
+const useClickOutsideRef = <T extends HTMLElement>(
+    ref: RefObject<T>,
+    callback: () => void
+) => {
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                callback();
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [callback, ref]);
+};
+
+export default useClickOutsideRef;
