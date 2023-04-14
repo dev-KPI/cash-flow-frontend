bd0c1a6e161fa0bece227b8606f9f38291cb4061
Artem Zabozhko <artemko526@gmail.com>
2023-04-14 20:33:30 +0200

    added object structure for requests

--- src/pages/Dashboard/HistoryCard/HistoryCard.tsx
+++ src/pages/Dashboard/HistoryCard/HistoryCard.tsx
@@ -8,34 +8,60 @@ import { RecentOperationDashboardCard } from "@components/RecentOperationsCards/
 //logic
 import { Link } from "react-router-dom";
 import { useContentSize } from "@hooks/useLayout";
+import { Omiter, addFieldToObject } from "@services/UsefulMethods/ObjectMethods";
+import { tmpObj } from "./testObj";
 
 
+type group_category_props = {
+    id: number,
+    title: string,
+    color: string,
+    icon: string
+}
+interface Transaction {
+    id: number;
+    amount: number;
+    time: string;
+    description: string;
+    category_group?: {
+        group?: group_category_props
+        category?: group_category_props
+    },
+    type: string
+}
 
 const HistoryCard: FC = () => {
     
-    const tmpObj = {
-        ColorBtn: '#FF6E01',
-        title: 'Test',
-        amount: 500.12,
-        time: '2023-03-29T13:51:50'
-    }
-    const isColorBtn: boolean = tmpObj.ColorBtn.match(/#([A-Z0-9]{1,6})/i) ? true : false
-
+     
     const {width} = useContentSize();
+    
+    const getMixedHistory = () => {
+        const expensesDTO: Transaction[] = [...tmpObj.expenses.map((el: Object) => 
+            Omiter(['id'], el))].map(el => addFieldToObject(el, 'type', 'expense'))
+        const replenishmentsDTO: Transaction[] = [...tmpObj.replenishments.map((el: Object) => 
+            Omiter(['id'], el))].map(el => addFieldToObject(el, 'type', 'replenishment'))
+    
+        const HistoryArray: Transaction[] = [...expensesDTO, ...replenishmentsDTO]
+        return(HistoryArray.sort((a, b) => {
+            const dateA = new Date(a.time).getTime();
+            const dateB = new Date(b.time).getTime();
+            return dateA - dateB;
+        }))
+    }
 
     const getRecentActivities = () => {
-        // const rangeActivities = width < 1440 ? 3 : 6
-        let res: ReactNode[] = [];
-        for (let i = 0; i < 6; i++){
-            res.push( 
-            <li key={i + 'adsa'}>
-                <RecentOperationDashboardCard 
-                ColorBtn={tmpObj.ColorBtn}
-                title={tmpObj.title}
-                amount={tmpObj.amount}
-                time={tmpObj.time}/> 
-            </li>)
-        }
+        let res: ReactNode[] = getMixedHistory().map((el, i) => {
+            return <>
+                <li key={i + 'adsa'}>
+                    <RecentOperationDashboardCard
+                    type={el.type === 'expense' ? 'expense' : 'replenishment'}
+                    ColorBtn={el.category_group?.category?.color || '#80D667'}
+                    title={el.category_group?.category?.title || 'Salary'}
+                    amount={el.amount}
+                    time={el.time}/> 
+                </li>
+            </>
+        })
         return res
     }
 
@@ -44,7 +70,7 @@ const HistoryCard: FC = () => {
             <h3>Recent Activity</h3>
             <ul>
                 {getRecentActivities()}
-                <li>
+                <li key='239k'>
                     <Link to={'/'}>
                         <div className={classes.ViewMore}>
                             <p>View More</p>
---
+++ src/pages/Dashboard/HistoryCard/testObj.ts
@@ -0,0 +1,138 @@
+export const tmpObj = JSON.parse(JSON.stringify({
+    "expenses": [
+        {
+            "id": 1,
+            "time": "2023-04-10T13:26:54",
+            "description": "Groceries",
+            "amount": 50.0,
+            "category_group": {
+                "group": {
+                    "id": 5,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 5,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                }
+            }
+        },
+        {
+            "id": 2,
+            "time": "2023-04-10T13:26:59",
+            "description": "Groceries",
+            "amount": 32.0,
+            "category_group": {
+                "group": {
+                    "id": 2,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 4,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                }
+            }
+        },
+        {
+            "id": 3,
+            "time": "2023-04-10T13:46:59",
+            "description": "Groceries",
+            "amount": 432.0,
+            "category_group": {
+                "group": {
+                    "id": 2,
+                    "title": "Book",
+                    "color": "#FF6E01",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 4,
+                    "title": "Book",
+                    "color": "#FF6E01",
+                    "icon": "link.com"
+                }
+            }
+        },
+        {
+            "id": 1,
+            "time": "2023-04-10T13:26:54",
+            "description": "Groceries",
+            "amount": 50.0,
+            "category_group": {
+                "group": {
+                    "id": 5,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 5,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                }
+            }
+        },
+        {
+            "id": 2,
+            "time": "2023-04-10T13:26:59",
+            "description": "Groceries",
+            "amount": 32.0,
+            "category_group": {
+                "group": {
+                    "id": 2,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 4,
+                    "title": "Book",
+                    "color": "#0f6f8f",
+                    "icon": "link.com"
+                }
+            }
+        },
+        {
+            "id": 3,
+            "time": "2023-04-10T13:46:59",
+            "description": "Groceries",
+            "amount": 432.0,
+            "category_group": {
+                "group": {
+                    "id": 2,
+                    "title": "Book",
+                    "color": "#FF6E01",
+                    "icon": "link.com"
+                },
+                "category": {
+                    "id": 4,
+                    "title": "Book",
+                    "color": "#FF6E01",
+                    "icon": "link.com"
+                }
+            }
+        }
+    ],
+    "replenishments": [
+        {
+            "id": 1,
+            "amount": 1000,
+            "time": "2023-04-10T13:26:56",
+            "description": "Groceries"
+        },
+        {
+            "id": 2,
+            "amount": 10400,
+            "time": "2023-04-10T13:29:56",
+            "description": "Groceries"
+        }
+    ]
+}))
