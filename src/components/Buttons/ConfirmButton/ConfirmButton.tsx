2d68a817d38ee06b657221fad1e14e68afd54711
Artem Zabozhko <artemko526@gmail.com>
2023-06-02 13:45:15 +0200

    Added params to button

--- src/components/Buttons/ConfirmButton/ConfirmButton.tsx
+++ src/components/Buttons/ConfirmButton/ConfirmButton.tsx
@@ -8,6 +8,8 @@ import PreLoader from '@components/PreLoader/PreLoader';
 interface IConfirmButtonProps {
     btnWidth?: number
     btnHeight?: number
+    titleFontSize?: number
+    titleFontWeight?: number
     type: 'submit' | 'button' | 'add' | 'refuse' | 'none',
     background?: 'solid' | 'outline'
     callback: () => void
@@ -16,14 +18,17 @@ interface IConfirmButtonProps {
     className?: string
 }
 
-const ConfirmButton: FC<IConfirmButtonProps> = ({ type, btnWidth = 100, btnHeight = 50, callback, isPending, background, title = '', className}) => {
+const ConfirmButton: FC<IConfirmButtonProps> = ({ titleFontWeight, titleFontSize, type, btnWidth = 100, btnHeight = 50, callback, isPending, background, title = '', className}) => {
 
     const [isAnimation = false, setIsAnimation] = useState<boolean>();
 
     const setStartHover = () => setIsAnimation(true)
     const setEndHover = () => setIsAnimation(false)
 
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => callback() 
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        callback()
+    } 
     const getIcon = () => {
         return type === 'submit' ? <i className="bi bi-check2"></i>
             : type === 'add' ? <i style={{ fontSize: '24px' }} className="bi bi-plus"></i>
@@ -59,7 +64,11 @@ const ConfirmButton: FC<IConfirmButtonProps> = ({ type, btnWidth = 100, btnHeigh
                 className={`${classes.ConfirmButton} ${className}`}>
                 <div className={classes.wrapperbtn}>
                     {getIcon()}
-                    <p style={{display: 'flex', alignItems: 'center'}}>{title ? title : 'Confirm'}</p>
+                    <p style={{
+                        display: 'flex', 
+                        alignItems: 'center',  
+                        fontSize: titleFontSize! > 0 ? titleFontSize + '' : '',
+                        fontWeight: titleFontWeight! > 0 ? titleFontWeight + 'px' : ''}}>{title ? title : 'Confirm'}</p>
                 </div>
             </button>
         </ButtonHover> 
