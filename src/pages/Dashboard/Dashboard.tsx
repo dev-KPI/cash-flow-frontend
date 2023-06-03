609cf52fe877c78ed3925f5fd9ba2f6368207b08
pest2811@gmail.com <pest2811@gmail.com>
2023-06-04 01:22:33 +0300

    add chart cards to pages

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,24 @@
 import React, { ReactNode, useCallback, useEffect, useState } from 'react';
 
+//logic
+import { useGetCategoryExpensesQuery, useGetCategoryExpensesTotalQuery } from '@store/UserCategoryExpenseApiSlice/UserCategoryExpenseApiSlice';
+
 //UI
 import classes from './Dashboard.module.css'                                        //global components
 import MonthPicker from '@components/MonthPicker/MonthPicker';
 import OperationCard from '@components/OperationCard/OperationCard';
 
-import UserExpenseCard from '@pages/Dashboard/UserExpenseCard/UserExpenseCard';            //local components
+import ChartCard from '@components/ChartCard/ChartCard';            //local components
 import UserExpenseGraphCard from '@pages/Dashboard/UserExpenseGraph/UserExpenseGraphCard';
 import UserAccountCard from '@pages/Dashboard/UserAccountCard/UserAccountCard';
 import UserHistoryCard from '@pages/Dashboard/UserHistoryCard/UserHistoryCard';
 import UserCategoriesCard from '@pages/Dashboard/UserCategoriesCard/UserCategoriesCard';
 import UserGroupsCard from '@pages/Dashboard/UserGroupsCard/UserGroupsCard';
+import UserChartCard from './UserChartCard/UserChartCard';
 
 
 const Dashboard = () => {
-
+    
     return (<>
         <main id='DashboardPage'>
             <div className='dashboard__container'>
@@ -31,7 +35,7 @@ const Dashboard = () => {
                         <UserCategoriesCard />
                     </div>
                     <div className={classes.grid__topright}>
-                        <UserExpenseCard />
+                        <UserChartCard/>
                         <UserGroupsCard />
                     </div>
                     <UserAccountCard />
