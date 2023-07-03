3eed4cb38cd16254aa1931e9d7ad82e2e5e862d6
pest2811@gmail.com <pest2811@gmail.com>
2023-07-04 02:08:33 +0300

    add functional for toggling categories by group

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,69 +1,147 @@
-import React, { FC, useState, ReactNode } from "react";
+import React, { FC, useState, ReactNode, Fragment, useCallback, useEffect, useMemo } from "react";
+import { categoriesObj } from './categoriesObj';
+
+import uuid from 'react-uuid';
 //logic
 import { useAppSelector } from "@hooks/storeHooks/useAppStore";
-import { NavLink } from "react-router-dom";
-import { CATEGORIES_PAGE } from "src/router/routes";
+import ICategory from "@models/ICategory";
 //UI
 import classes from './Categories.module.css'
 import CustomButton from "@components/Buttons/CustomButton/CustomButton";
 import CategoriesCard from "./CategoriesCard/CategoriesCard";
 import CategoryModal from "@components/ModalWindows/CategoryModal/CategoryModal";
 
+
+
+const Groups = [
+    {
+        "id": 0,
+        "title": "Its title",
+        "groups": {
+            "color_code": "#FF0000",
+            "icon_url": "bi bi-bank"
+        }
+    },
+    {
+        "id": 1,
+        "title": "Its group",
+        "groups": {
+            "color_code": "#99FF00",
+            "icon_url": "bi bi-camera"
+        }
+    },
+    {
+        "id": 2,
+        "title": "small",
+        "groups": {
+            "color_code": "#FF6600",
+            "icon_url": "bi bi-badge-vr"
+        }
+    },
+    {
+        "id": 3,
+        "title": "-_-",
+        "groups": {
+            "color_code": "#FF6600",
+            "icon_url": "bi bi-badge-vr"
+        }
+    },
+    {
+        "id": 4,
+        "title": "hahha",
+        "groups": {
+            "color_code": "#FF6600",
+            "icon_url": "bi bi-badge-vr"
+        }
+    },
+    {
+        "id": 5,
+        "title": "very long title",
+        "groups": {
+            "color_code": "#FF6600",
+            "icon_url": "bi bi-badge-vr"
+        }
+    },
+]
 const Categories: FC = () => {
 
     const actualTheme = useAppSelector(state => state.persistedThemeSlice.theme);
 
     const [groups = false, setGroups] = useState<boolean>();
+    const [categories, setCategories] = useState<ICategory[]>([])
+    const [selectedGroup, setSelectedGroup] = useState<number>(0);
     const [isCategoryModal = false, setIsCategoryModal] = useState<boolean>();
+    
+    const { categoriesJson } = categoriesObj;
+    
 
-    const initializePage = () => {
+    const initializeCategories = useCallback(() => {
+        const newCategories = categoriesJson.find(item => item.id === selectedGroup)?.categories
+        if (newCategories) {
+            setCategories(newCategories);
+        }
+    }, [selectedGroup, categoriesJson])
 
-    }
-
+    useEffect(() => {
+        initializeCategories()
+    }, [initializeCategories])
     const openModal = () => {
         setIsCategoryModal(!isCategoryModal)
-        console.log(isCategoryModal)
     }
-
-    const setActiveLinkClasses = (isActive: boolean) => {
-        let res = isActive ? classes.active : classes.item;
-        res += (actualTheme === 'dark' && isActive) ? ' ' + classes.shadowLink : ' ';
-        return res
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+        setSelectedGroup(+event.target.value)
     }
 
     const getGroups = () => {
         let res: ReactNode[] = [];
-        for (let i = 0; i < 3; i++) {
-            res.push(
-                <li key={'12sf3' + i}>
-                    <NavLink
-                        className={({ isActive }) => setActiveLinkClasses(isActive)}
-                        to="/123"><h4>Family</h4></NavLink>
-                </li>
-            )
+        Groups.slice(0,5).map((el, i) => {
+            const title = el.title.length > 12 ? el.title.slice(0, 9).trim() + "..." : el.title
+            return res.push(
+                <Fragment key={'12sf3' + i}>
+                    <input
+                        type='radio'
+                        value={el.id}
+                        checked={selectedGroup === el.id}
+                        id={`group-item-${i}`}
+                        name='group'
+                        onChange={handleChange}
+                        className={classes.inputItem} />
+                    <label
+                        htmlFor={`group-item-${i}`}
+                        className={classes.groupTitle}
+                    >{title}</label>
+                </Fragment>
+            )}
+        )
+        if (Groups.length > 4) {
+            res.push(<CustomButton
+                isPending={false}
+                callback={() => console.log(1)}
+                icon="none"
+                type="primary"
+                children="View More"
+                // disableScale={true}
+                className={classes.groupMoreButton}
+            />)
         }
         return res;
     }
 
-    const getCategories = () => {
-        let res: ReactNode[] = [];
-        for (let i = 0; i < 7; i++) {
-            res.push(
-                <li className={classes.item} key={'12sf3' + i}>
-                    <CategoriesCard
-                        id={i}
-                        color="#eee333"
-                        title="Family"
-                        icon="bi bi-joystick" />
-                </li>)
-        }
-        return res
-    }
+    const getCategories = useMemo<JSX.Element[]>(() => {
+        return categories.map((item, i) =>
+            <CategoriesCard
+                key={uuid()} 
+                id={i}
+                color={item.color}
+                title={item.title}
+                icon={item.icon} />
+        )
+    }, [categories])
 
     const getCategoriesModal = () => {
         return <CategoryModal
-        setIsCategoryModalOpen={setIsCategoryModal}
-        isCategoryModalOpen={isCategoryModal}
+            setIsCategoryModalOpen={setIsCategoryModal}
+            isCategoryModalOpen={isCategoryModal}
         />
     }
 
@@ -72,13 +150,9 @@ const Categories: FC = () => {
         <main id='CategoriesPage'>
             <div className={classes.CategoriesPage__container}>
                 <h3 className={classes.pageTitle}>Categories</h3>
-                <div className={classes.CategoriesNav}>
-                    <nav className={classes.NavWrapper}>
-                        <ul className={classes.navbarUl}>
-                            {getGroups()}
-                        </ul>
-                    </nav>
-                </div>
+                <nav className={classes.NavWrapper}>
+                    {getGroups()}
+                </nav>
                 <div className={classes.addCategory}>
                     <div className={classes.upSide}>
                         <h5 className={classes.CategoryTitle}>Category</h5>
@@ -88,12 +162,12 @@ const Categories: FC = () => {
                             icon="add"
                             type="primary"
                             children="Create new category"
-                            className={`btn-primary`} />
+                            />
                     </div>
                     <div className={classes.line}></div>
                 </div>
                 <ul className={classes.CategoriesBox}>
-                    {getCategories()}
+                    {getCategories}
                 </ul>
             </div>
         </main>
