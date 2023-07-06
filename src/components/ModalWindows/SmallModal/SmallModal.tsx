594df812ac7aa4af04bea551b5a4e68071914fb4
pest2811@gmail.com <pest2811@gmail.com>
2023-07-06 23:01:57 +0300

    modal fix (not done)

--- src/components/ModalWindows/SmallModal/SmallModal.tsx
+++ src/components/ModalWindows/SmallModal/SmallModal.tsx
@@ -1,5 +1,5 @@
 import CloseButton from '@components/Buttons/CloseButton/CloseButton';
-import React, {FC, SetStateAction, Dispatch, useRef, ReactNode} from 'react';
+import React, {FC, SetStateAction, Dispatch, useRef, ReactNode, useState, useEffect} from 'react';
 import { useOnClickOutside } from 'usehooks-ts'
 //UI
 import classes from './SmallModal.module.css';
@@ -13,6 +13,7 @@ interface ISmallModalProps {
     buttonRef?: React.RefObject<HTMLElement>
 }
 const SmallModal: FC<ISmallModalProps> = ({ active, setActive, children, className, title, buttonRef }) => {
+    const [isFadeOut, setIsFadeOut] = useState<boolean>(false)
     const ref = useRef(null);
     useOnClickOutside(ref, (event) => {
         if (buttonRef) {
@@ -21,21 +22,46 @@ const SmallModal: FC<ISmallModalProps> = ({ active, setActive, children, classNa
         } else {
             setActive(false)
         }
-        
     })
+    useEffect(() => {
+        if (!active)
+            setIsFadeOut(true) 
+    }, [active])
+    // const handleButtonClick = (newVisibility: boolean) => {
+    //     if (newVisibility) {
+    //         setActive(true)
+    //     } else {
+    //         setIsFadeOut(true)
+    //     }
+    // }
+    const handleAnimationEnd = (e: React.AnimationEvent<HTMLDivElement>) => {
+        if (e.animationName === 'fadeOut') { 
+            setActive(false)
+            setIsFadeOut(false)
+            console.log(1);
+        }
+    }
+    const handleCloseModal = () => { 
+        // if (active)
+            setActive(false)
+    }
     return (
-        <div
-            className={`${className ? className : ''} + ${active ? `${classes.modal} ${classes.active}` : classes.modal}`}
-            onClick={(e) => e.stopPropagation()}
-            ref={ref}>
-            <div className={classes.modalHeader}>
-                <h5 className={classes.title}>{title}</h5>
-                <CloseButton size={24} closeHandler={() => { setActive(false) }} />
-            </div>
-            <div className={classes.modal__content}>
-                {children}
-            </div>
-        </div>
+        <>
+            {active && 
+                <div
+                    className={`${className ? className : ''} + ${isFadeOut ? `${classes.modal} ${classes.cardFadeOut}` : classes.modal}`}
+                    onClick={(e) => e.stopPropagation()}
+                    onAnimationEnd={handleAnimationEnd}
+                    ref={ref}>
+                    <div className={classes.modalHeader}>
+                        <h5 className={classes.title}>{title}</h5>
+                        <CloseButton size={24} closeHandler={() => { setActive(false) }} />
+                    </div>
+                    <div className={classes.modal__content}>
+                        {children}
+                    </div>
+                </div>}
+        </>
     );
 };
 
