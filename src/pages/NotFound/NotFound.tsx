2affe7fb8322f7be15e730e7f12f5623afb34b0d
Artem Zabozhko <artemko526@gmail.com>
2023-07-13 10:21:35 +0300

    NotFoundPage

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//UI
+import classes from './NotFound.module.css';
+import robot from '@assets/404Robot.png';
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (<>
+        <div className={classes.NotFound}>
+            <img className={classes.robot} width="300px" src={robot} alt="Robot" />
+            <div className={classes.rightSide}>
+                <h5 className={classes.title}>Oops!... Page not found!</h5>
+                <Link to={'/dashboard'}>
+                    <p className={classes.backBtn}>Back to homepage</p>
+                </Link>
+            </div>
+        </div>
+    </>)
+}
+
+export default NotFound
