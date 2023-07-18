69cd7f7927146e262568668f06543c632901fa1d
pest2811@gmail.com <pest2811@gmail.com>
2023-07-19 00:25:49 +0300

    add groupListItem loader

--- src/pages/Groups/GroupListItem/GroupIListItem.tsx
+++ src/pages/Groups/GroupListItem/GroupIListItem.tsx
@@ -8,6 +8,7 @@ import classes from './GroupListItem.module.css'
 import SmallModal from '@components/ModalWindows/SmallModal/SmallModal';
 import EditGroupModal from '@components/ModalWindows/GroupModal/EditGroupModal';
 import userIcon from '@assets/user-icon.svg';
+import GroupListItemLoader from './GroupListItemLoader';
 
 
 
@@ -24,6 +25,7 @@ interface IGroupItemProps {
 const GroupItem: FC<IGroupItemProps> = ({ id, title, description, icon, adminName, adminEmail, color, memberIcons }) => {
     const [isGroupModal, setIsGroupModal] = useState<boolean>(false);
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [loading = true, setLoading] = useState<boolean>();
     const buttonRef = useRef(null);
     const navigate = useNavigate()
 
@@ -50,7 +52,9 @@ const GroupItem: FC<IGroupItemProps> = ({ id, title, description, icon, adminNam
             :
             <i className={"bi bi-people"}></i>
     }
-
+    setTimeout(() => {
+        setLoading(false)
+    }, 1500);
     return (
         <div className={classes.wrapper}>
             <SmallModal
@@ -86,56 +90,57 @@ const GroupItem: FC<IGroupItemProps> = ({ id, title, description, icon, adminNam
                         </li>
                     </ul>}
             />
-        <EditGroupModal
+            <EditGroupModal
                 setIsGroupModalOpen={setIsGroupModal}
                 isGroupModalOpen={isGroupModal}
             />
-        <Link
-            key={uuid()}
-            to={`/group/${id}`}
-        >
-            <div className={classes.group}>
-                <div className={classes.inner}>
-                    <h4 className={classes.title}>{title}</h4>
-                    <div className={classes.content}>
-                        <div className={classes.details}>
-                            <div className={classes.icon}
-                                style={{ backgroundColor: color }}
-                            >{getAdminIcon()}</div>
-                            <div className={classes.info}>
-                                <h6 className={classes.ownerName}>{adminName}</h6>
-                                <p className={classes.ownerEmail}>{adminEmail}</p>
-                            </div>
-                        </div>
-                        <div className={classes.description}>{description}</div>
-                        <div className={classes.contentBottom}>
-                            <div className={classes.members}>
-                                {getMemberIcons()}
-                                {memberIcons.length > 3 ?
-                                    <div className={classes.avatar}>
-                                        <div className={classes.avatarLeftMembers}
-                                            style={{ backgroundColor: color }}></div>
-                                        <p className={classes.leftMembers}
-                                            style={{ color: color }}
-                                        >+{memberIcons.length - 3}
-                                        </p>
+            <Link
+                key={uuid()}
+                to={`/group/${id}`}
+            >
+                <div className={classes.group}>
+                    {loading ? <GroupListItemLoader /> : 
+                        <div className={classes.inner}>
+                            <h4 className={classes.title}>{title}</h4>
+                            <div className={classes.content}>
+                                <div className={classes.details}>
+                                    <div className={classes.icon}
+                                        style={{ backgroundColor: color }}
+                                    >{getAdminIcon()}</div>
+                                    <div className={classes.info}>
+                                        <h6 className={classes.ownerName}>{adminName}</h6>
+                                        <p className={classes.ownerEmail}>{adminEmail}</p>
                                     </div>
-                                    : null
-                                }
+                                </div>
+                                <div className={classes.description}>{description}</div>
+                                <div className={classes.contentBottom}>
+                                    <div className={classes.members}>
+                                        {getMemberIcons()}
+                                        {memberIcons.length > 3 ?
+                                            <div className={classes.avatar}>
+                                                <div className={classes.avatarLeftMembers}
+                                                    style={{ backgroundColor: color }}></div>
+                                                <p className={classes.leftMembers}
+                                                    style={{ color: color }}
+                                                >+{memberIcons.length - 3}
+                                                </p>
+                                            </div>
+                                            : null
+                                        }
+                                    </div>
+                                    <button className={classes.moreBtn}
+                                        ref={buttonRef}
+                                        onClick={(e) => { e.preventDefault(); setIsModalOpen(!isModalOpen) }}>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </button>
+                                </div>
                             </div>
-                            <button className={classes.moreBtn}
-                                ref={buttonRef}
-                                onClick={(e) => { e.preventDefault(); setIsModalOpen(!isModalOpen) }}>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </button>
-                        </div>
-                    </div>
+                        </div>}
                 </div>
-            </div>
-        </Link>
-    </div>   
+            </Link>
+        </div>   
     );
 };
 
