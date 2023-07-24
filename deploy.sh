aeaa8509644e448b1625a3a15314b02d19579c20
Artem Zabozhko <artemko526@gmail.com>
2023-07-24 15:46:55 +0300

    Deploy done

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+source .env
+
 echo "Switching to branch master"
 git checkout dev
 
@@ -5,6 +7,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server"
-scp -r build/* root@46.101.150.9:/var/www/cash-money-front
+scp -r build/* $SHELL_ROOT:/var/www/cash-money-front
 
 echo "Done!"
