d307054516153e3072f1dddcf10e9dab3a4876e6
Artem Zabozhko <artemko526@gmail.com>
2023-07-29 18:32:44 +0300

    Added initialization of Group default id

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,38 @@ import { useGetUserAuthStatusQuery } from '@store/Controllers/UserController/Use
 import { useActionCreators } from '@hooks/storeHooks/useAppStore';
 import { ThemeActions } from '@store/UI_store/ThemeSlice/ThemeSlice';
 import { UserSliceActions } from '@store/User/UserSlice';
+import { useGetCurrentUserGroupsQuery } from '@store/Controllers/GroupsController/GroupsController';
+import { GroupSliceActions } from '@store/Group/GroupSlice';
 import PageGlobalLoader from '@components/PageGlobalPreloader/PageGlobalPreloader';
 
 
 const App: React.FC = () => {
 
-    const {data: AuthStatus, isError: isAuthError, isFetching: isAuthFetching} = useGetUserAuthStatusQuery(null);
+    const {data: AuthStatus, isError: isAuthError, isLoading: isAuthLoading} = useGetUserAuthStatusQuery(null);
+    const {data: UserGroups, isError: isUserGroupsError, isLoading: isUserGroupsLoading} = useGetCurrentUserGroupsQuery(null);
 
     const ThemeDispatch = useActionCreators(ThemeActions);
     const UserSliceDispatch = useActionCreators(UserSliceActions);
+    const GroupsSliceDispatch = useActionCreators(GroupSliceActions);
 
-    const initializeAuthorization = useCallback(() => {
-        if (!isAuthFetching){
+    const initializeApp = useCallback(() => {
+        ThemeDispatch.initializeTheme()
+        if (!isUserGroupsLoading && !isUserGroupsError){
+            GroupsSliceDispatch.setDefaultGroup(UserGroups?.user_groups[0].group.id)
+        }
+        if (!isAuthLoading){
             UserSliceDispatch.setIsAuth(AuthStatus);
         } 
-    }, [AuthStatus, isAuthError, isAuthFetching])
+    }, [AuthStatus, isAuthError, isAuthLoading, 
+        UserGroups, isUserGroupsError, isUserGroupsLoading])
 
     useEffect(() => {
-        initializeAuthorization()
-        ThemeDispatch.initializeTheme()
-    },[ThemeDispatch, UserSliceDispatch, AuthStatus, initializeAuthorization]);
+        initializeApp()
+    },[initializeApp]);
 
-    return (
-        <Router />
-    )
+    return (<>
+        {(!isAuthLoading && !isUserGroupsLoading) ? <Router /> : <PageGlobalLoader/>}
+    </>)
 };
 
 export default App;
