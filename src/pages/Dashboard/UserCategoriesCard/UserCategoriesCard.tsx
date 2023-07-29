0b0af0c85bb0c159e6a0ee342bd4ea9eca93c44e
Artem Zabozhko <artemko526@gmail.com>
2023-07-29 18:31:14 +0300

    Categories connected to API

--- src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
+++ src/pages/Dashboard/UserCategoriesCard/UserCategoriesCard.tsx
@@ -6,6 +6,11 @@ import { handleWrap } from '@services/UsefulMethods/UIMethods';
 import IExpense from '@models/IExpense';
 import { useGetExpensesByGroupQuery } from '@store/Controllers/ExpensesController/ExpensesController';
 import { useGetCurrentUserGroupsQuery } from '@store/Controllers/GroupsController/GroupsController';
+import { useGetCategoriesByGroupQuery } from '@store/Controllers/CategoriesController/CategoriesController';
+import { useAppSelector } from '@hooks/storeHooks/useAppStore';
+import { IMonthPickerState } from '@store/UI_store/MonthPickerSlice/MonthPickerInterfaces';
+import DateService from '@services/DateService/DateService';
+import IGroupState from '@store/Group/GroupInterfaces';
 //UI
 import CategoriesCardItem from '@components/CategoriesCardItem/CategoriesCardItem';
 import classes from './UserCategoriesCard.module.css'
@@ -14,14 +19,17 @@ import ExpenseModal from '@components/ModalWindows/ExpenseModal/ExpenseModal';
 import ViewMoreModal from '@components/ModalWindows/ViewMoreModal/ViewMoreModal';
 import CategoryModal from '@components/ModalWindows/CategoryModal/CategoryModal';
 import SpecialButton from '@components/Buttons/SpeciaButton/SpecialButton';
+import { skipToken } from '@reduxjs/toolkit/dist/query';
 
 
 
 const UserCategoriesCard = () => {
-    // const [categories, setCategories] = useState<ISortedCategoryItem[]>([]);
+
+    const MonthPickerStore = useAppSelector<IMonthPickerState>(store => store.MonthPickerSlice)
+    const GroupsStore = useAppSelector<IGroupState>(store => store.GroupSlice)
     const [totalItems, setTotalItems] = useState<number>(11);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [selectedGroup, setSelectedGroup] = useState<number>(0);
+    const [pageGroup, setGroupPage] = useState<number>(0);
+    const [selectedGroup, setSelectedGroup] = useState<number>(GroupsStore.defaultGroup);
     const [idExpenseModalOpen, setIdExpenseModalOpen] = useState<number>(-1);
     const [isExpenseModalOpen, setIsExpenseModalOpen] = useState<boolean>(false);
     const [isCategoryModalOpen, setIsCategoryModalOpen] = useState<boolean>(false);
@@ -29,15 +37,10 @@ const UserCategoriesCard = () => {
     const [squareRef, { width, height }] = useElementSize<HTMLUListElement>();
 
     const { data: UserGroups, isLoading: isGroupsLoading, isError: isGroupsError } = useGetCurrentUserGroupsQuery(null);
-    const { data: ExpensesByGroup, isLoading: isExpensesLoading, isError: isExpensesError } = useGetExpensesByGroupQuery({group_id: selectedGroup, year_month: '2023-07'})
-    // const { categoriesByGroup } = json;
-    // let groups = categoriesByGroup.map(a => a.title);
-    
-    // const initializeCategories = useCallback(() => {
-    //     const newCategories = categoriesByGroup.find(item => item.title === groups[groupIndex])?.categories
-    //     if (newCategories)
-    //         setCategories(newCategories);
-    // }, [groupIndex])
+    const { data: UserCategoriesByGroup, isLoading: isCategoriesByGroupLoading, isError: isCategoriesByGroupError } = useGetCategoriesByGroupQuery(selectedGroup);
+    const { data: ExpensesByGroup, isLoading: isExpensesLoading, isError: isExpensesError } = useGetExpensesByGroupQuery(
+        {group_id: selectedGroup, period: {year_month: DateService.getYearMonth(MonthPickerStore.currentYear, MonthPickerStore.currentMonth)}}
+    )
 
     const initializeHandleWrapper = useCallback(()=> {
         handleWrap(classes.list, classes.wrapped, classes.specialItem, 2);
@@ -54,13 +57,42 @@ const UserCategoriesCard = () => {
         autoHandleCloseModal])
 
     const getCategories = (categories: IExpense[]) => {
-        return categories.map((item, i) => 
-            <CategoriesCardItem 
-            key={i} 
-            setIdModalOpen={setIdExpenseModalOpen}
-            setIsModalOpen={setIsExpenseModalOpen}
-            expense={item}/>
-        )
+        if(categories.length > 0){
+            return categories.map((item, i) => 
+                <CategoriesCardItem 
+                key={i} 
+                categoryId={idExpenseModalOpen}
+                setIdModalOpen={setIdExpenseModalOpen}
+                setIsModalOpen={setIsExpenseModalOpen}
+                expense={item}/>
+            )
+        } else {
+            return UserCategoriesByGroup?.categories_group.map((item, i) => 
+                <CategoriesCardItem 
+                key={i} 
+                categoryId={idExpenseModalOpen}
+                setIdModalOpen={setIdExpenseModalOpen}
+                setIsModalOpen={setIsExpenseModalOpen}
+                expense={{
+                    id: i,
+                    descriptions: 'none',
+                    amount: 0,
+                    time: 'none',
+                    category_group: {
+                        group: {
+                            id: 0,
+                            title: 'none'
+                        },
+                        category: {
+                            id: item.category.id,
+                            title: item.category.title
+                        },
+                        color_code: item.color_code,
+                        icon_url: item.icon_url
+                    }
+                }}/>
+            )
+        }
     }
     const getExpenseModal = () => {
         return <ExpenseModal
@@ -96,37 +128,37 @@ const UserCategoriesCard = () => {
     }, [ExpensesByGroup, totalItems])
 
     const handleNextGroup = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (UserGroups && !UserGroups.user_groups[selectedGroup + 1].group) return;
-        setSelectedGroup(selectedGroup + 1); 
+        if (UserGroups && UserGroups.user_groups[pageGroup + 1]) {
+            setGroupPage(pageGroup + 1);
+            setSelectedGroup(UserGroups.user_groups[pageGroup + 1].group.id);
+        } 
     }
 
-    const handlePrevGroup =(e: React.MouseEvent<HTMLButtonElement>) => {
-        if (UserGroups && !UserGroups.user_groups[selectedGroup - 1].group) return;
-        setSelectedGroup(selectedGroup - 1); 
+    const handlePrevGroup = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (UserGroups && UserGroups.user_groups[pageGroup - 1]) {
+            setGroupPage(pageGroup - 1);
+            setSelectedGroup(UserGroups.user_groups[pageGroup - 1].group.id);
+        } 
     }
-    
-    setTimeout(() => {
-        setLoading(false)
-    }, 1500);
 
     return (
         <div className={classes.categories}>
             {getExpenseModal()}
             {getViewMoreModal()}
             {getCategoryModal()}
-            {loading ? <UserCategoriesCardLoader /> : <>            
+            {isGroupsLoading ? <UserCategoriesCardLoader /> : <>            
                 <div className={classes.inner}>
                     <div className={classes.top}>
                         <h3 className={classes.title}>Categories
                             <span className={classes.categoryName}>
-                                ({UserGroups?.user_groups[selectedGroup].group.title})
+                                ({UserGroups?.user_groups[pageGroup].group.title})
                             </span>
                         </h3>
                         <div className={classes.nav}>
                             <button
                                 type="submit"
                                 onClick={handlePrevGroup}
-                                disabled={!UserGroups?.user_groups[selectedGroup - 1]}
+                                disabled={!UserGroups?.user_groups[pageGroup - 1]}
                                 className={classes.btn + ' ' + classes.previous}
                                 >
                                 <i id='chevron' className="bi bi-chevron-left"></i>
@@ -134,7 +166,7 @@ const UserCategoriesCard = () => {
                             <button
                                 type="submit"
                                 onClick={handleNextGroup}
-                                disabled={!UserGroups?.user_groups[selectedGroup + 1]}
+                                disabled={!UserGroups?.user_groups[pageGroup + 1]}
                                 className={classes.btn + ' ' + classes.next}
                                 >
                                 <i id='chevron' className="bi bi-chevron-right"></i>
