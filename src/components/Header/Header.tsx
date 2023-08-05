d45c577f66f0aefe77085a2c0c47593569e5ce37
pest2811@gmail.com <pest2811@gmail.com>
2023-08-06 01:15:49 +0300

    login responsive fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
 //types
-import {FC, ReactNode} from 'react';
-//hooks
-import { useWindowSize } from 'usehooks-ts';
-import { useCallback } from 'react';
+import {FC} from 'react';
 
 //UI
 import PcHeader from './PcHeader/PcHeader';
 import MobileHeader from './MobileHeader/MobileHeader';
 import { useGetCurrentUserInfoQuery } from '@store/Controllers/UserController/UserController';
-import { Omiter } from '@services/UsefulMethods/ObjectMethods';
 import PageGlobalLoader from '@components/PageGlobalPreloader/PageGlobalPreloader';
 
 const Header: FC = () => {
