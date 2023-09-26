836e5a76b04e8f207868cd93188f941b0e576629
pest2811@gmail.com <pest2811@gmail.com>
2023-09-27 01:42:19 +0300

    add setting theme color based on windows theme (only on first render)

--- src/store/UI_store/ThemeSlice/ThemeSlice.ts
+++ src/store/UI_store/ThemeSlice/ThemeSlice.ts
@@ -3,12 +3,15 @@ import { createSlice } from '@reduxjs/toolkit'
 //types
 import { IThemeState } from './ThemeInterfaces';
 
+const isSystemDarkMode = () =>
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
 const initialState: IThemeState = {
-    theme: new Date().getHours() > 19 ? 'dark' : 'light',
-    cardBackgroundColor: new Date().getHours() > 19 ? '#FFFFFF' : '0F0F0F',
-    backgroundColor: new Date().getHours() > 19 ? '#151515' : '#F8F8FF',
-    mainTextColor: new Date().getHours() > 19 ? '#EAEAEA' : '#333333',
-    textColor: new Date().getHours() > 19 ? '#9BABC5' : '#7A7A9D'
+    theme: isSystemDarkMode() ? 'dark' : 'light',
+    cardBackgroundColor: isSystemDarkMode() ? '#FFFFFF' : '0F0F0F',
+    backgroundColor: isSystemDarkMode() ? '#151515' : '#F8F8FF',
+    mainTextColor: isSystemDarkMode() ? '#EAEAEA' : '#333333',
+    textColor: isSystemDarkMode() ? '#9BABC5' : '#7A7A9D'
 }
 
 export const ThemeSlice = createSlice({
